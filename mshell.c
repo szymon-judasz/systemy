6d988c3dce9c8689911eec2964f0d31b465a901e
Szymon Juudasz <email@email.com>
2015-10-18 21:21:41 +0200

    Save after second classes

--- mshell.c
+++ mshell.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #include "string.h"
 #include "config.h"
 #include "siparse.h"
 #include "utils.h"
-#include <errno.h>
 
+
+void runCommand(command _command);
+int findEndOfLine(char* tab, int pos, int size); // looking for '\n' char in tab[size] begining from pos
+// and returning -1 if not found, or x>=pos if found
 
 int
 main(int argc, char *argv[])
@@ -14,49 +21,75 @@ main(int argc, char *argv[])
 	line * ln;
 	command *com;
 
-	/*ln = parseline("ls -las | grep k | wc ; echo abc > f1 ;  cat < f2 ; echo abc >> f3\n");
-	printparsedline(ln);
-	printf("\n");
-	com = pickfirstcommand(ln);
-	printcommand(com,1);
-	printf("just before second command\n");
-	ln = parseline("sleep 3 &");
-	printparsedline(ln);
-	printf("\n");
+	int printPrompt = 1;
+	struct stat statbuffer;
 	
-	ln = parseline("echo  & abc >> f3\n");
-	printparsedline(ln);
-	printf("\n");
-	com = pickfirstcommand(ln);
-	printcommand(com,1);*/
+	int continue_flag = 1; // warning: set but not reset
+	char buffer[2 * MAX_LINE_LENGTH + 1]; // bufor dla read
+	char* bufferptr = buffer;
+	char* lineBegin;
+	char* lineEnd;
+	char line[MAX_LINE_LENGTH];
 
-
-	int continue_flag = 1;
-	char buffer[MAX_LINE_LENGTH + 1]; 
+	int status = fstat(STDIN_FILENO, &statbuffer);
+	if(status!=0)
+	{// some errors
+		
+	} else
+	{
+		if(!S_ISCHR(statbuffer.st_mode))
+			printPrompt = 0;
+	}
 	while(continue_flag)
 	{
 		int bytesRead;
-		write(0, PROMPT_STR, sizeof(PROMPT_STR)); // 1. wypisz prompt na std
-		bytesRead = read(0, buffer, MAX_LINE_LENGTH); // 2. wczytaj linie z std
+		if(printPrompt)
+			write(STDOUT_FILENO, PROMPT_STR, sizeof(PROMPT_STR)); // 1. wypisz prompt na std
+		
+		bufferptr = buffer;
+		bytesRead = read(STDIN_FILENO, buffer, MAX_LINE_LENGTH); // 2. wczytaj linie z std
 		//write(0, buffer, bytesRead);
-		if(bytesRead == -1){
-			// error handling
-			return -1;
-		}
-		if(bytesRead == 0)
+		if(bytesRead == -1)	return -1;
+		if(bytesRead == 0) return 0;
+		
+		//bufferptr += bytesRead;
+		
+		int lastPos = 0;
+		while(lastPos != 0)
 		{
-			return 0;
-		}
-		buffer[bytesRead] = 0;
-		ln = parseline(buffer);
-		if(!ln) // 4.2 parsowanie zakonczone bledem
-		{
-			write(0, SYNTAX_ERROR_STR, sizeof(SYNTAX_ERROR_STR)); // 1. wypisz prompt na std
-			continue;
+			lineBegin = bufferptr;
+			lastPos = findEndOfLine(buffer, bufferptr - buffer, MAX_LINE_LENGTH);
+			buffer[lastPos] = 0;
+			bufferptr = buffer + lastPos + 1;
+			
+			
+		
+		
+			//buffer[bytesRead] = 0;
+			ln = parseline(lineBegin);
+			if(!ln) // 4.2 parsowanie zakonczone bledem
+			{
+				write(STDOUT_FILENO, SYNTAX_ERROR_STR, sizeof(SYNTAX_ERROR_STR)); // 1. wypisz prompt na std
+				continue;
+			}
+		
+			pipeline _pipeline = *(ln->pipelines);
+			command _command = **_pipeline;
+		
+			runCommand(_command);
+		
+		
+			int result = 0;
+			waitpid(-1,&result,0);
+			result = WEXITSTATUS(result);
 		}
 		
-		pipeline _pipeline = *(ln->pipelines);
-		command _command = **_pipeline;
+	}
+
+}
+
+void runCommand(command _command)
+{
 		int childpid = fork();
 		if(childpid == 0)
 		{ // if kid
@@ -77,16 +110,20 @@ main(int argc, char *argv[])
 			{
 				strcat(buffer, ": exec error\n");
 			}
-			write(0, buffer, strlen(buffer));
+			write(STDERR_FILENO, buffer, strlen(buffer));
 			
 			exit(EXEC_FAILURE);
 		}
-		
-		int result = 0;
-		waitpid(-1,&result,0);
-		result = WEXITSTATUS(result);
-		
-		
-	}
+}
 
+int findEndOfLine(char* tab, int pos, int size) // looking for '\n' char in tab[size] begining from pos
+{	// and returning -1 if not found, or x>=pos if found
+	int p = pos;
+	while(p<size)
+	{
+		if(tab[p] == '\n')
+			return p;
+		p++;
+	}
+	return -1;
 }
